1b404da33c8c3a02ddaabbbd6b83b8408e3a0e83
Benson Mburu <nguruben@gmail.com>
2015-10-26 19:49:49 +0300

    added main actitvity

--- app/src/main/java/ml/dev2dev/realestate/Splashactivity.java
+++ app/src/main/java/ml/dev2dev/realestate/Splashactivity.java
@@ -1,6 +1,8 @@
 package ml.dev2dev.realestate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +10,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Splashactivity extends AppCompatActivity {
-
+private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashactivity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
+
+         /*
+          * Showing splash screen with a timer. This will be useful when you
+          * want to show case your app logo / company
+          */
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splashactivity.this, index.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
             }
-        });
+        }, SPLASH_TIME_OUT);
+    }
     }
 
-}
